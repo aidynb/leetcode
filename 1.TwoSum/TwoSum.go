3a4313f0a5bd198e0e0b820bce0f074d0aaba5e3
aidynb <aidyn.baigarinov@gmail.com>
2021-11-24 21:42:43 +0600

    add 1.TwoSum

---
+++ 1.TwoSum/TwoSum.go
@@ -0,0 +1,14 @@
+package lc
+
+func TwoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, num := range nums {
+		if value, exists := m[target-num]; exists {
+			return []int{value, i}
+		} else {
+			m[num] = i
+		}
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aidynb/leetcode
+
+go 1.17
